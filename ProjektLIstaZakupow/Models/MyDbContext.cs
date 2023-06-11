e8e7054c4b33a3bb6aa260a35be1af1365bd2d43
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2023-06-11 16:12:04 +0200

    Koniec

--- ProjektLIstaZakupow/Models/MyDbContext.cs
+++ ProjektLIstaZakupow/Models/MyDbContext.cs
@@ -14,7 +14,8 @@ namespace ProjektLIstaZakupow.Models
         {
             modelBuilder.Entity<Produkt>().HasData(new Produkt { Id = 1, CzyKupione = false, Nazwa = "Marchewka", Rodzaj = "Warzywa" },
                 new Produkt { Id = 2, CzyKupione = false, Nazwa = "Kurczak", Rodzaj = "Mięso" },
-                new Produkt { Id = 3, CzyKupione = false, Nazwa = "Cukierki", Rodzaj = "Słodycze" });
+                new Produkt { Id = 3, CzyKupione = false, Nazwa = "Cukierki", Rodzaj = "Słodycze" },
+                new Produkt { Id = 4, CzyKupione = true, Nazwa = "Chipsy", Rodzaj = "Inne" });
             modelBuilder.Entity<Produkt>().ToTable("Produkty");
         }
     }
